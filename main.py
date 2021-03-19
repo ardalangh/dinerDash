49a8c9e0d21c11eda4fd46ba700d2ca1f866e86c
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-19 16:10:49 -0700

    fix err

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ bg = pygame.image.load("./assets/bg.png").convert()
 
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             running = False
 
     
