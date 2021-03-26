23b1f8fe0814c5e3d96ab6ffb09729e0103ed4bb
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-26 15:47:48 -0700

    draw function written

--- Table.py
+++ Table.py
@@ -4,12 +4,16 @@ class Table:
     file_path = "/assets/tableSingle.png" 
 
 
-    def __init__(self, cap, screen):
+    def __init__(self, cap):
         if cap is not in Table.possible_caps:
             raise ValueError("The capacity of your table needs to in [2, 4, 6, 8]")
-        self.screen = screen # screen variable of pygame
+    
         self.cap = cap    # The cap of the table (int)
         self.empty = True # Table is empty (bool)
         self.guest_at = 0 # How many people are at this table (int)
         self.num = None   # The number of the table (int)
 
+
+    def draw(self, screen):
+        table = pygame.image.load(Table.file_path).convert() 
+        screen.blit(table, [10,10])
