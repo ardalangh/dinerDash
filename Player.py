671857f8ec810a9d4f8bba0c4790e8b85adcf385
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-05-21 16:02:06 -0700

    test dir added

--- Player.py
+++ Player.py
@@ -13,10 +13,8 @@ class Player:
         self.player_rect = self.player_loaded.get_rect()
         self.debug = True
 
-
     def get_file_path(self):
         return f"./assets/girlWalk{self.dir}{self.img_counter}.png"
-
 
     def moveLeft(self):
         self.x -= 10
@@ -30,36 +28,34 @@ class Player:
 
     def draw(self, screen):
         if self.debug:
-            pygame.draw.rect(screen , (0,0,0), self.player_rect, 1)
+            pygame.draw.rect(screen, (0, 0, 0), self.player_rect, 1)
         screen.blit(self.player_loaded, [self.x, self.y])
 
     def update(self, obstacles):
-        if (self.moving):
-
-            if (self.movingTo[0] < self.x and all([obstacle.collidepoint(self.player_rect.midleft) for obstacle in obstacles])):
+        if self.moving:
+            if (self.movingTo[0] < self.x and all(
+                    [obstacle.collidepoint(self.player_rect.midleft) for obstacle in obstacles])):
                 self.dir = "LEFT"
                 self.x -= 5
                 self.player_rect = self.player_rect.move(-5, 0)
 
-            if (self.movingTo[0] > self.x and all([obstacle.collidepoint(self.player_rect.midright) for obstacle in obstacles])):
+            if (self.movingTo[0] > self.x and all(
+                    [obstacle.collidepoint(self.player_rect.midright) for obstacle in obstacles])):
                 self.dir = "RIGHT"
                 self.x += 5
-                self.player_rect = self.player_rect.move(5,0)
-            if (self.movingTo[1] < self.y and  all([obstacle.collidepoint(self.player_rect.midtop) for obstacle in obstacles])):
+                self.player_rect = self.player_rect.move(5, 0)
+            if (self.movingTo[1] < self.y and all(
+                    [obstacle.collidepoint(self.player_rect.midtop) for obstacle in obstacles])):
                 self.y -= 5
                 self.player_rect = self.player_rect.move(0, -5)
-            if (self.movingTo[1] > self.y and all([obstacle.collidepoint(self.player_rect.midbottom) for obstacle in obstacles])):
+            if (self.movingTo[1] > self.y and all(
+                    [obstacle.collidepoint(self.player_rect.midbottom) for obstacle in obstacles])):
                 self.y += 5
-                self.player_rect = self.player_rect.move(0,5)
-
-
-
+                self.player_rect = self.player_rect.move(0, 5)
 
             # self.img_counter = (self.img_counter + 1) % 4
-            if (abs(self.movingTo[1] - self.y) < 5 and abs(self.movingTo[0] - self.x) < 5):
+            if abs(self.movingTo[1] - self.y) < 5 and abs(self.movingTo[0] - self.x) < 5:
                 self.moving = False;
-
-
 
     def move_helper(self, dest, vel):
         """
@@ -71,28 +67,15 @@ class Player:
         """
         res = ()
         # check if x is not at dest
-        if (self.x != dest[0]) :
+        if self.x != dest[0]:
             if self.x < dest[0]:
                 res[0] = vel * 1
             else:
                 res[0] = vel * -1
         # check if y is not at dest
-        if (self.y != dest[1]) :
+        if self.y != dest[1]:
             if self.y < dest[1]:
                 res[1] = vel * 1
             else:
                 res[1] = vel * -1
         return res
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ Test/TestPlayer.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+
+
+
+class TestPlayer(unittest.TestCase):
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
